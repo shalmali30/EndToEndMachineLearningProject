dbd6db78773183933d12786999a46d0ee6cabd2c
shalmali30 <limbkar.shalmali30@gmail.com>
2024-03-19 19:01:17 +0100

    Data transformation added in modular coding

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -2,7 +2,7 @@
 
 from mlProject.constants import *  # first calling the path of yaml files which are in the constants folder
 from mlProject.utils.common import read_yaml, create_directories  # then call the other functions created for reading the yamlfile (read_yaml), and creating dir etc from utils
-from mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 class ConfigurationManager:
     def __init__(
         self,
@@ -48,3 +48,15 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -14,3 +14,8 @@ class DataValidationConfig:
     STATUS_FILE: str
     unzip_data_dir: Path
     all_schema: dict   # Yaml file always return dictionary format , so datatype is Dict
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
